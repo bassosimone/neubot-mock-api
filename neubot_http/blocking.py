79456492c599234379620cc6da35c7362e6c491a
Simone Basso <bassosimone@gmail.com>
2015-02-01 13:11:31 +0100

    Implement and use output queue

--- neubot_http/blocking.py
+++ neubot_http/blocking.py
@@ -9,6 +9,7 @@
 
 from .mixins import HTTPRequestHandlerMixin
 from .mixins import HTTPServerMixin
+from .outqueue import HTTPOutputQueue
 from .parser import HTTPParser
 
 class _RequestHandler(HTTPRequestHandlerMixin):
@@ -31,21 +32,19 @@ class _RequestHandler(HTTPRequestHandlerMixin):
     def handle(self):
         """ Called to handle the request """
         parser = HTTPParser()
+        outqueue = HTTPOutputQueue()
         while True:
             data = self.request.recv(65535)
             if data:
                 parser.feed(data)
             else:
                 parser.eof()
-            while True:
+            result = parser.parse()
+            while result:
+                outqueue.insert_data(self.emit(result))
                 result = parser.parse()
-                if not result:
-                    break
-                to_send = self.emit(result)
-                if not to_send:
-                    continue
-                for piece in to_send:
-                    self.request.sendall(piece)
+            while outqueue:
+                self.request.sendall(outqueue.get_next_chunk())
             if not data:
                 break
 
