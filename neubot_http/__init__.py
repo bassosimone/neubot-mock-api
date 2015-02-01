dac4a8b7360a594008738c1f16c2c53d51271ea8
Simone Basso <bassosimone@gmail.com>
2015-02-01 18:28:45 +0100

    Fix usage example

--- neubot_http/__init__.py
+++ neubot_http/__init__.py
@@ -8,47 +8,31 @@
 """
  Neubot HTTP library.
 
- Basic usage:
+ Example usage:
 
-     import logging
      import neubot_http
-     import socket
+     import json
 
-     def foobar(message):
-         ''' Handles the /debug URL '''
-         logging.debug("type: %s", message["type"])
-         logging.debug("method: %s", message["method"])
-         logging.debug("url: %s", message["url"])
-         logging.debug("protocol: %s", message["protocol"])
-         for key in message["headers"]:
-             logging.debug("header: %s => %s", key, message["headers"][key])
-         for chunk in message["body"]:
-             logging.debug("chunk: %s", chunk)
-
-         # Python 3
-         #yield from neubot_http.serializer.compose_response("200", "Ok", {
-         #    "Content-Type": "application/json",
-         #}, "{}")
-
-         subgen = neubot_http.serializer.compose_response("200", "Ok", {
-             "Content-Type": "application/json",
-         }, "{}")
-         for chunk in subgen:
-             yield chunk
-
-     # Commented out params are default values
-     neubot_http.serve({
-         #"backlog": 128,
-         #"family": socket.AF_INET,
-         "hostname": "127.0.0.1",    # default: ""
-         #"mode": "async",           # otherwise: forked, threaded, single
-         #"port": 8080,
-         "rootdir": "/var/www",      # default: "" (= no rootdir)
-         "routes": {                 # default: {}
-             "/debug": foobar,
-             "/debug/x": foobar
+     def simple(request):
+         ''' Handles /URL '''
+         response = {
+             "method": request.method,
+             "url": request.url,
+             "protocol": request.protocol,
+             "headers": request.headers,
+             "body": request.body_as_string()
          }
-     })
+         yield neubot_http.serializer.compose_response("200", "Ok", {
+             "Content-Type": "application/json",
+         }, json.dumps(response, indent=4))
+
+     def main():
+         ''' Main function '''
+         neubot_http.serve({
+             "routes": {
+                 "/simple": simple,
+             }
+         })
 """
 
 from .serve import serve
