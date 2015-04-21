bb94532bd9afda99f2cbd87566b36fe1ca0eb060
Simone Basso <bassosimone@gmail.com>
2015-04-21 19:16:18 +0200

    Make /api/state comet work

--- neubot_scheduler/api.py
+++ neubot_scheduler/api.py
@@ -10,29 +10,41 @@
 import cgi
 import json
 import logging
+import os
 
 from . import backend
 from . import http
+from . import utils
 
 class StateManager(object):
     """ State manager """
 
     def __init__(self):
-        self.pending = []
+        self.comet_pending = []
+        self.events = {
+            "since": utils.timestamp(),
+            "pid": os.getpid(),
+        }
+        self.current = "idle"
+        self.tsnap = utils.opaque_time()
 
     def serialize(self):
         """ Serialize state manager """
         return http.writer.compose_response("200", "Ok", {
             "Content-Type": "application/json",
-        }, "{}")
+        }, json.dumps({
+            "events": self.events,
+            "current": self.current,
+            "t": self.tsnap,
+        }))
 
     def comet_wait(self, connection):
-        self.pending.append(connection)
+        self.comet_pending.append(connection)
 
     def comet_trigger(self):
-        pending = self.pending
-        self.pending = []
-        for connection in pending:
+        comet_pending = self.comet_pending
+        self.comet_pending = []
+        for connection in comet_pending:
             try:
                 connection.write(self.serialize())
             except (KeyboardInterrupt, SystemExit):
--- neubot_scheduler/http/writer.py
+++ neubot_scheduler/http/writer.py
@@ -40,6 +40,7 @@ def compose(first_line, headers, before, filep, after):
             logging.debug("> %s: %s", name, value)
             header = "%s: %s\r\n" % (name, value)
             yield header
+    yield "Access-Control-Allow-Origin: *\r\n"
     yield "\r\n"
 
     if before:
