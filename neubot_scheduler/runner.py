c7109c41bc14b8378115cd21469f58193fee1093
Simone Basso <bassosimone@gmail.com>
2015-04-26 19:15:10 +0200

    Implement basic runner

--- neubot_scheduler/runner.py
+++ neubot_scheduler/runner.py
@@ -7,13 +7,88 @@
 
 """ Runner """
 
-from . import http
+import datetime
+import logging
+import os
+import subprocess
+import tempfile
+
+from . import utils
 
 class Runner(object):
     """ Runner class """
 
-    def run(self, connection, test, streaming):
-        """ Trigger the runner """
-        connection.write(http.writer.compose_response("200", "Ok", {
-            "Content-Type": "application/json",
-        }, "{}"))
+    @staticmethod
+    def _make_pendingdir(basedir):
+        """ Make name of directory in which test data is kept """
+        pendingdir = os.path.join(basedir, "pending", datetime.datetime.now()
+                                                                .isoformat())
+        rwx______ = int("700", 8)  # py2/py3 portable
+        os.mkdir(pendingdir, rwx______)
+        return pendingdir
+
+    def _quickly_write_file(self, name, content):
+        """ Quickly write file in pendingdir """
+        with open(os.path.join(self.pendingdir, name), "w+") as filep:
+            filep.write("%s\n" % content)
+
+    def __init__(self, complete, schedule, basedir, command_line, **kwargs):
+        self.complete = complete
+        self.basedir = basedir
+
+        self.pendingdir = self._make_pendingdir(self.basedir)
+        logging.debug("runner: pendingdir: %s", self.pendingdir)
+
+        self.max_runtime = kwargs.get("max_runtime", 30)
+        self.started = utils.timestamp()
+        self.schedule = schedule
+        self.stdin = open(os.path.join(self.pendingdir, "stdin"), "w+b")
+        bytes_for_stdin = kwargs.get("bytes_for_stdin")
+        if bytes_for_stdin:
+            self.stdin.write(bytes_for_stdin)
+            self.stdin.seek(0, os.SEEK_SET)
+        self.stdout = open(os.path.join(self.pendingdir, "stdout"), "w+b")
+        self.stderr = open(os.path.join(self.pendingdir, "stderr"), "w+b")
+
+        self.proc = subprocess.Popen(command_line, close_fds=True,
+                                     stdin=self.stdin, stdout=self.stdout,
+                                     stderr=self.stderr)
+
+        logging.debug("runner: subprocess started with pid %d", self.proc.pid)
+        self._quickly_write_file("pid", self.proc.pid)
+        self._quickly_write_file("started", self.started)
+        self._quickly_write_file("status", "running")
+        self.schedule(5.0, 0, self.periodic_task_, ())
+
+    def periodic_task_(self):
+        """ Periodically monitor subprocess """
+        current_time = utils.timestamp()
+        exitcode = self.proc.poll()
+        if exitcode is not None:
+            logging.debug("runner: subprocess %d exited", self.proc.pid)
+            logging.debug("runner: exitcode is %d", exitcode)
+            self._quickly_write_file("status", "exited")
+            self._quickly_write_file("exitcode", exitcode)
+            self.final_state_()
+        elif current_time - self.started > self.max_runtime:
+            logging.debug("runner: terminating subprocess %d", self.proc.pid)
+            self.proc.terminate()
+            # Assume that once killed the process will terminate
+            self._quickly_write_file("status", "killed")
+            self.final_state_()
+        else:
+            logging.debug("runner: subprocess %d still running", self.proc.pid)
+            self.schedule(5.0, 0, self.periodic_task_, ())
+
+    def final_state_(self):
+        """ Clear opened resources """
+        self.proc = None
+        self.stdin.close()
+        self.stdout.close()
+        self.stderr.close()
+        if self.complete:
+            self.complete(self)
+
+    def get_pendingdir(self):
+        """ Return the base directory """
+        return self.pendingdir
---
+++ neubot_scheduler/test/runner.py
@@ -0,0 +1,41 @@
+#
+# This file is part of Neubot <https://www.neubot.org/>.
+#
+# Neubot is free software. See AUTHORS and LICENSE for more
+# information on the copying conditions.
+#
+
+""" Runner tests """
+
+import unittest
+import logging
+import sys
+import sched
+import time
+
+if __name__ == "__main__":
+    sys.path.insert(0, ".")
+
+from neubot_scheduler.runner import Runner
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
+class NormalUsageCase(unittest.TestCase):
+    """ Normal usage case """
+
+    def test_normal_usage(self):
+        """ Tests normal usage """
+        scheduler = sched.scheduler(time.time, time.sleep)
+
+        Runner(None, scheduler.enter, "./var/lib/neubot/scheduler", [
+            "/usr/bin/mtr", "--report-wide", "8.8.8.8"
+        ])
+
+        Runner(None, scheduler.enter, "./var/lib/neubot/scheduler", [
+            "/usr/bin/python"
+        ], bytes_for_stdin=b"\xe4Antani\xff\xfe")
+
+        scheduler.run()
+
+if __name__ == "__main__":
+    unittest.main()
