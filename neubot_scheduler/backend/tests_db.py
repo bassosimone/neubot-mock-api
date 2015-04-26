642b9406686eca9f081c11270ab35b2fc54117e1
Simone Basso <bassosimone@gmail.com>
2015-04-26 13:53:56 +0200

    Rename and modify specs_manager.py -> tests_db.py

---
+++ neubot_scheduler/backend/tests_db.py
@@ -0,0 +1,36 @@
+#
+# This file is part of Neubot <https://www.neubot.org/>.
+#
+# Neubot is free software. See AUTHORS and LICENSE for more
+# information on the copying conditions.
+#
+
+""" Network tests DB """
+
+import json
+import os
+
+class TestsDB(object):
+    """ Network tests DB class """
+
+    def __init__(self, basedir):
+        self.basedir = basedir
+
+    def read_one(self, name):
+        """ Read single test descriptor """
+        if name.startswith("."):
+            return
+        path = os.path.join(self.basedir, name)
+        if not os.path.isfile(path):
+            return
+        with open(path, "r") as filep:
+            return json.load(filep)
+
+    def read_all(self):
+        """ Read all tests descriptors """
+        result = {}
+        for name in os.listdir(self.basedir):
+            value = self.read_one(name)
+            if value is not None:
+                result[name] = value
+        return result
