01b51cd6843b148b6e72d2fa1d7d8ce18bd8cc89
Simone Basso <bassosimone@gmail.com>
2015-04-29 14:54:03 +0200

    Add stub of command dump

--- neubot_scheduler/__main__.py
+++ neubot_scheduler/__main__.py
@@ -12,20 +12,28 @@ import logging
 import sys
 
 from .frontend import Frontend
+from . import command_dump
 
 def main():
     """ Main function """
 
     level = logging.WARNING
+    mode = ""
     try:
-        options, _ = getopt.getopt(sys.argv[1:], "v")
+        options, _ = getopt.getopt(sys.argv[1:], "v", ["dump"])
     except getopt.error:
-        sys.exit("Usage: neubot-scheduler [-v]")
+        sys.exit("Usage: neubot-scheduler [-v] [--dump]")
     for name, _ in options:
         if name == "-v":
             level = logging.DEBUG
+        elif name == "--dump":
+            mode = "dump"
 
     logging.basicConfig(format="%(message)s", level=level)
+
+    if mode == "dump":
+        command_dump.dump(sys.stdout)
+        sys.exit(0)
 
     frontend = Frontend()
     frontend.loop()
---
+++ neubot_scheduler/command_dump.py
@@ -0,0 +1,12 @@
+#
+# This file is part of Neubot <https://www.neubot.org/>.
+#
+# Neubot is free software. See AUTHORS and LICENSE for more
+# information on the copying conditions.
+#
+
+""" Dump everything in the database """
+
+def dump(filep):
+    """ Dump everything in the database """
+    raise NotImplementedError
