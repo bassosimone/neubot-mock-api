b549ce314b9c19757e9f1c60181a80e0314287f4
Simone Basso <bassosimone@gmail.com>
2015-05-05 16:15:47 +0200

    Remove http

--- neubot_scheduler/http/test/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# This file is part of Neubot <https://www.neubot.org/>.
-#
-# Neubot is free software. See AUTHORS and LICENSE for more
-# information on the copying conditions.
-#
-
-""" Neubot HTTP library tests """
